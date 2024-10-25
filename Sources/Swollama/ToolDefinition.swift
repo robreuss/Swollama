75d94a1787207e2865797440005aee10ae2e3b43
Marcus Ziadé <guitaripod@icloud.com>
2024-10-25 18:42:13 +0700

    Add ToolDefinition and related structures for tools

---
+++ Sources/Swollama/ToolDefinition.swift
@@ -0,0 +1,103 @@
+import Foundation
+
+/// Represents a tool that can be used by the model
+public struct ToolDefinition: Codable, Sendable {
+    /// The type of the tool
+    public let type: String
+    /// The function definition
+    public let function: FunctionDefinition
+    
+    public init(type: String = "function", function: FunctionDefinition) {
+        self.type = type
+        self.function = function
+    }
+}
+
+/// Represents a function definition for a tool
+public struct FunctionDefinition: Codable, Sendable {
+    /// The name of the function
+    public let name: String
+    /// Description of what the function does
+    public let description: String
+    /// The parameters the function accepts
+    public let parameters: Parameters
+    
+    public init(
+        name: String,
+        description: String,
+        parameters: Parameters
+    ) {
+        self.name = name
+        self.description = description
+        self.parameters = parameters
+    }
+}
+
+/// Represents the parameters for a function
+public struct Parameters: Codable, Sendable {
+    /// The type of the parameters object
+    public let type: String
+    /// The properties of the parameters
+    public let properties: [String: PropertyDefinition]
+    /// Required parameter names
+    public let required: [String]
+    
+    public init(
+        type: String = "object",
+        properties: [String: PropertyDefinition],
+        required: [String]
+    ) {
+        self.type = type
+        self.properties = properties
+        self.required = required
+    }
+}
+
+/// Represents a property in a parameter definition
+public struct PropertyDefinition: Codable, Sendable {
+    /// The type of the property
+    public let type: String
+    /// Description of the property
+    public let description: String
+    /// Allowed values for enum types
+    public let enumValues: [String]?
+    
+    public init(
+        type: String,
+        description: String,
+        enumValues: [String]? = nil
+    ) {
+        self.type = type
+        self.description = description
+        self.enumValues = enumValues
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case type
+        case description
+        case enumValues = "enum"
+    }
+}
+
+/// Represents a tool call made by the model
+public struct ToolCall: Codable, Sendable {
+    /// The function that was called
+    public let function: FunctionCall
+    
+    public init(function: FunctionCall) {
+        self.function = function
+    }
+}
+
+/// Represents a function call made by the model
+public struct FunctionCall: Codable, Sendable {
+    /// The name of the function that was called
+    public let name: String
+    /// The arguments provided to the function
+    public let arguments: String
+    
+    public init(name: String, arguments: String) {
+        self.name = name
+        self.arguments = arguments
+    }
+}
