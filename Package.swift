33883cf390fbb4d5634f97f8462c790288968cd7
Marcus Ziadé <guitaripod@icloud.com>
2024-10-25 13:27:10 +0700

    Add initial package setup and .gitignore file

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Swollama",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Swollama",
+            targets: ["Swollama"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Swollama"),
+        .testTarget(
+            name: "SwollamaTests",
+            dependencies: ["Swollama"]
+        ),
+    ]
+)
